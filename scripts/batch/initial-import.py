77792ff7c00c792c324985ca381f23a8ae50e427
Brock Fanning <brockfanning@gmail.com>
2019-01-22 13:39:25 +0400

    Progress

--- scripts/batch/initial-import.py
+++ scripts/batch/initial-import.py
@@ -9,9 +9,9 @@ import pandas as pd
 import numpy as np
 
 # For more readable code below.
-HEADER_YEAR_TIDY = 'Year'
-HEADER_VALUE_TIDY = 'Value'
-FOLDER_DATA_CSV_TIDY = 'data'
+HEADER_YEAR = 'Year'
+HEADER_VALUE = 'Value'
+FOLDER_DATA_CSV = 'data'
 
 """
 Normatization notes:
@@ -47,17 +47,27 @@ At this point print output for tweaks.
 
 YEARS = ['2015', '2016', '2017']
 
-def tidy_blank_dataframe():
+def get_disaggregation_column(category):
+    ret = 'Unknown'
+    if category == 'Male' or category == 'Female':
+        ret = 'Sex'
+    if category == 'Rural' or category == 'Urban':
+        ret = 'Location'
+    if '-' in category or ' to ' in category:
+        ret = 'Age'
+    print category + ' ' + ret
+
+def blank_dataframe():
     """This starts a blank dataframe with our required tidy columns."""
 
     # Start with two columns, year and value.
-    blank = pd.DataFrame({HEADER_YEAR_WIDE:[], HEADER_VALUE_TIDY:[]})
+    blank = pd.DataFrame({HEADER_YEAR:[], HEADER_VALUE:[]})
     # Make sure the year column is typed for integers.
-    blank[HEADER_YEAR_WIDE] = blank[HEADER_YEAR_WIDE].astype(int)
+    blank[HEADER_YEAR] = blank[HEADER_YEAR].astype(int)
 
     return blank
 
-def tidy_csv(csv):
+def foo_csv(csv):
 
     try:
         tidy = tidy_dataframe(df, metadata['indicator_variable'])
@@ -66,7 +76,7 @@ def tidy_csv(csv):
         return False
 
     try:
-        tidy_path = os.path.join(FOLDER_DATA_CSV_TIDY, csv_filename)
+        tidy_path = os.path.join(FOLDER_DATA_CSV, csv_filename)
         tidy.to_csv(tidy_path, index=False, encoding='utf-8')
         print('Converted ' + csv_filename + ' to tidy format.')
     except Exception as e:
@@ -92,7 +102,7 @@ def main():
     status = True
 
     # Create the place to put the files.
-    os.makedirs(FOLDER_DATA_CSV_TIDY, exist_ok=True)
+    os.makedirs(FOLDER_DATA_CSV, exist_ok=True)
 
     # Read the Excel spreadsheet into a dataframe.
     excel_opts = {
@@ -103,22 +113,48 @@ def main():
     }
     df = pd.read_excel('SDG_eng.xlsx', **excel_opts)
 
-    # Ignore rows with no yearly data.
-    years = ['2015', '2016', '2017']
-    df = df.dropna(subset=years, how='all')
+    # Some indicators are already properly disaggregated in the source data.
+    already_disaggregated = [
+        '2.2.2',
+        '8.5.2',
+        '8.6.1'
+    ]
 
     # Merge 'Global' and 'National' to a 'Category' column.
     df['Category'] = np.where(df.National.isnull(), df.Global, df.National)
     df = df.drop(labels=['Global', 'National'], axis='columns')
 
+    # Remove rows without value in Category.
+    df = df.dropna(subset=['Category'], how='all')
+
+    # NOTE: The total number of rows should not change after this point. We
+    # reset the index now for logical sequential indexes.
+    df = df.reset_index()
+    df = df.drop(labels=['index'], axis='columns')
+
     # Figure out where each indicator starts and set the ID.
-    starting_rows = {}
+    indicators = {}
     for row in df.iterrows():
         category = row[1]['Category']
         id = indicator_id(category)
         if id:
-            starting_rows[id] = row[0]
+            indicators[id] = {
+                'start': row[0]
+            }
+            # Replace the cell data with just the id number.
             df.set_value(row[0], 'Category', id)
+    # Also set the end rows.
+    last_id = False
+    for id in indicators:
+        if last_id:
+            indicators[last_id]['end'] = (indicators[id]['start'] - 1)
+        last_id = id
+    # Set the last end row.
+    for id in indicators:
+        if 'end' not in indicators[id]:
+            indicators[id]['end'] = df.last_valid_index()
+        # For ease later, add the number of rows.
+        indicators[id]['num_rows'] = indicators[id]['end'] - indicators[id]['start'] + 1
 
     # Remove "age" and "aged".
     df = df.replace('aged', '', regex=True)
@@ -127,7 +163,90 @@ def main():
     # Remove whitespace from Category values.
     df['Category'] = df['Category'].str.strip()
 
-    print(df['Category'])
+    # More normalization replacements.
+    replacements = {
+        'boy': 'Male',
+        'girl': 'Female',
+        'urban': 'Urban',
+        'rural': 'Rural'
+    }
+    for row in df.iterrows():
+        category = row[1]['Category']
+        category = category.lower()
+
+        # Skip rows with commas, as those seem to be fine already.
+        if ',' in category:
+            continue
+
+        for replacement in replacements:
+            if replacement in category:
+                df.set_value(row[0], 'Category', replacements[replacement])
+
+    # Figure out which rows are disaggregations. Only look for Male/Female.
+    for id in indicators:
+        start = indicators[id]['start']
+        end = indicators[id]['end']
+        num_rows = indicators[id]['num_rows']
+        # Skip indicators with only one row, as they have no disaggregation.
+        if num_rows == 1:
+            continue
+
+        # Take a slice of the main dataframe for this indicator.
+        indicator_df = df[start:end + 1]
+
+        # For indicators that already have sufficient disaggregation, we just
+        # convert the commas to pipes.
+        if id in already_disaggregated:
+            for row in indicator_df.iterrows():
+                index = row[0]
+                category = row[1]['Category']
+                category = category.replace(',', '|')
+                df.set_value(index, 'Category', category)
+
+        # Since we're only looking for Male/Female disaggregation, we can also
+        # skip any rows that have 1 or less instances of 'Male' or 'Female'.
+        num_male = indicator_df['Category'].str.count('Male').sum()
+        num_female = indicator_df['Category'].str.count('Female').sum()
+        if num_male < 2 and num_female < 2:
+            continue
+
+        first_male_found = False
+        first_female_found = False
+        last_non_gender_category = False
+        for row in indicator_df.iterrows():
+            index = row[0]
+            category = row[1]['Category']
+            if category == 'Male':
+                if not first_male_found:
+                    first_male_found = True
+                    continue
+            if category == 'Female':
+                if not first_female_found:
+                    first_female_found = True
+                    continue
+            if category != 'Male' and category != 'Female':
+                last_non_gender_category = category
+            else:
+                # We need to update the main dataframe by combining this
+                # Male/Female with whatever the last non-gender category was,
+                # separated by a pipe.
+                disaggregated_category = '|'.join([category, last_non_gender_category])
+                df.set_value(index, 'Category', disaggregated_category)
+
+    # Finally loop through the indicators and create CSV files.
+    for id in indicators:
+        start = indicators[id]['start']
+        end = indicators[id]['end']
+        num_rows = indicators[id]['num_rows']
+
+        # Take a slice of the main dataframe for this indicator.
+        indicator_df = df[start:end + 1]
+        print(indicator_df)
+        break
+
+        csv_df = tidy_blank_dataframe()
+
+
 
     return status
 
